a9ae06a1ee0f350ab86315ac9fa8e559cc6e70e5
kingsley <kingsblog9ja@gmail.com>
2025-07-24 11:50:10 +0100

    update index.js and .gitignore

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 
+ console.log('MONGO_URI:', process.env.MONGO_URI);
 
   mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB Atlas'))
